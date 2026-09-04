794ee706ee7749e7689df519b95976f4dbcb69d0
dertilon <94222107+dertilon@users.noreply.github.com>
2026-09-04 06:59:04 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -1,7 +1,8 @@
 // Lista de usuarios y contraseñas permitidos
 const USUARIOS_AUTORIZADOS = [
-    { usuario: "1002322", contrasena: "mantorema" },
-    { usuario: "1005479", contrasena: "mantorema" },
-    { usuario: "1005474", contrasena: "mantorema" },
-    { usuario: "1001480", contrasena: "mantorema" }
+    { usuario: "1002322", contrasena: "1234" },
+    { usuario: "1005479", contrasena: "1234" },
+    { usuario: "1005474", contrasena: "1234" },
+    { usuario: "1005321", contrasena: "1234" },
+    { usuario: "1001480", contrasena: "1234" }
 ];
